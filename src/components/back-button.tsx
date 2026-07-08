544915690e2dc82629f5384e6fc67d88153b20b0
Oscar Castro <oscar.a.castro818@gmail.com>
2026-07-08 17:54:02 -0500

    add new parameter to back button to force fallback href

--- src/components/back-button.tsx
+++ src/components/back-button.tsx
@@ -6,10 +6,23 @@ import { ArrowLeft } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 
-export function BackButton({ fallbackHref, label = "Back" }: { fallbackHref: string; label?: string }) {
+export function BackButton({
+  fallbackHref,
+  label = "Back",
+  preferFallback = false,
+}: {
+  fallbackHref: string;
+  label?: string;
+  preferFallback?: boolean;
+}) {
   const router = useRouter();
 
   function goBack() {
+    if (preferFallback) {
+      router.push(fallbackHref);
+      return;
+    }
+
     if (window.history.length > 1) {
       router.back();
       return;
